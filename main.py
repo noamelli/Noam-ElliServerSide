97c11366f22aaa2bd3b5e9d98fcabeb849fe223e
noamelli <Noameli90@gmail.com>
2022-06-11 22:27:35 +0300

    updating

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def contact_us():
     return render_template('contactUs.html')
 
 
-@app.route('/assignment3')
-def assignment3():
+@app.route('/assignment3_1')
+def assignment3_1():
     user_info = {'FIRST Name': 'NOAM', 'Last Name': 'elli'}
     age=24.11        #there is a filter that round down the age
     hobbies=('music', 'cooking', 'food')
@@ -53,6 +53,33 @@ def assignment3():
                            age=age,
                            hobbies=hobbies
                            )
+@app.route('/assignment3_2')
+def assignment3_2():
+    return render_template('assignment3_2.html')
+
+
+
+
+
+@app.route('/searchForm', methods=['get'])
+def searchForm():
+    user_dict = {
+        {'user1': {'firstName': 'yosi', 'lastName': 'cohen', 'email': 'yosi@gmail.com'}},
+        {'user1': {'firstName': 'dana', 'lastName': 'omer', 'email': 'dana@gmail.com'}},
+        {'user1': {'firstName': 'neta', 'lastName': 'shalom', 'email': 'neta@gmail.com'}},
+        {'user1': {'firstName': 'noam', 'lastName': 'harel', 'email': 'noam@gmail.com'}},
+        {'user1': {'firstName': 'omer', 'lastName': 'shalev', 'email': 'omer@gmail.com'}}
+    }
+    return render_template('assignment3_2.html',user_dict=user_dict)
+
+
+
+
+
+# @app.route('/registrationForm', methods=['post'])
+# def login_func():
+#     return render_template('log_in.html')
+#
 
 
 
