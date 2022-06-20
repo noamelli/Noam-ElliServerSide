b5624575d87c3e429b4a0d9c35fc59d4ae2475a0
noamelli <Noameli90@gmail.com>
2022-06-20 10:17:07 +0300

    updating

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from flask import Flask, redirect, render_template
 from flask import url_for
 from flask import render_template
+from flask import request, session, jsonify
+from datetime import timedelta
 
 app = Flask(__name__)
+
+app.secret_key = '473'
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1440)
 
 
 @app.route('/')
 def root():
-    return redirect(url_for('webRoot'))
+    return redirect('/homepage')
 
-@app.route('/webRoot')
-def webRoot():
-    user_info = {'FIRST Name': 'NOAM', 'Last Name': 'elli'}
-    age=24.11        #there is a filter that round down the age
-    hobbies=('music', 'cooking', 'food')
-    #hobbies=()            #for checking what happens if i dont write any hobby
-    return render_template('assignment3_1.html',
-                           user_info=user_info,
-                           age=age,
-                           hobbies=hobbies
-                           )
 
 # root of our website
 @app.route('/homepage')
@@ -42,45 +37,90 @@ def contact_us():
     return render_template('contactUs.html')
 
 
+# assigment 3_1- the button of this page called "about me"
 @app.route('/assignment3_1')
 def assignment3_1():
     user_info = {'FIRST Name': 'NOAM', 'Last Name': 'elli'}
-    age=24.11        #there is a filter that round down the age
-    hobbies=('music', 'cooking', 'food')
-    #hobbies=()            #for checking what happens if i dont write any hobby
+    age = 24.11  # there is a filter that round down the age
+    hobbies = ('music', 'cooking', 'food')
+    # hobbies=()      #for checking what happens if i dont write any hobby: serves for qestion 3c in part B
     return render_template('assignment3_1.html',
                            user_info=user_info,
                            age=age,
                            hobbies=hobbies
                            )
+
+
+# assigment 3_2 - the button of this page called "search for" / " register"
+# search form
 @app.route('/assignment3_2')
 def assignment3_2():
     return render_template('assignment3_2.html')
 
 
+user_dict = {
+    'yosi': {'nickName': 'yosiC', 'lastName': 'cohen', 'email': 'yosi@gmail.com', 'password': '475333'},
+    'li': {'nickName': 'lico', 'lastName': 'coren', 'email': 'li90@gmail.com', 'password': '231111'},
+    'ido': {'nickName': 'idodo', 'lastName': 'dor', 'email': 'idor@gmail.com', 'password': '222444'},
+    'neta': {'nickName': 'dubi', 'lastName': 'meir', 'email': 'sati@gmail.com', 'password': '234235'},
+    'noam': {'nickName': 'noami', 'lastName': 'elli', 'email': 'noami@gmail.com', 'password': '123456'}
+}
 
 
-
-@app.route('/searchForm', methods=['get'])
+@app.route('/searchForm', methods=['GET'])
 def searchForm():
-    user_dict = {
-        {'user1': {'firstName': 'yosi', 'lastName': 'cohen', 'email': 'yosi@gmail.com'}},
-        {'user1': {'firstName': 'dana', 'lastName': 'omer', 'email': 'dana@gmail.com'}},
-        {'user1': {'firstName': 'neta', 'lastName': 'shalom', 'email': 'neta@gmail.com'}},
-        {'user1': {'firstName': 'noam', 'lastName': 'harel', 'email': 'noam@gmail.com'}},
-        {'user1': {'firstName': 'omer', 'lastName': 'shalev', 'email': 'omer@gmail.com'}}
-    }
-    return render_template('assignment3_2.html',user_dict=user_dict)
+    if 'fname' in request.args:
+        first_name = request.args['fname']
+        if first_name in user_dict:
+            return render_template('assignment3_2.html',
+                                   first_name=first_name,
+                                   last_name=user_dict[first_name]['lastName'],
+                                   nickName=user_dict[first_name]['nickName'],
+                                   email=user_dict[first_name]['email'])
+        elif (first_name == ""):
+            return render_template('assignment3_2.html',
+                                   user_dict=user_dict)
+        else:
+            return render_template('assignment3_2.html',
+                                   message='The user is not found!')
 
 
+# registration form
+@app.route('/registerForm', methods=['POST', 'GET'])
+def register_func():
+    if request.method == 'POST':
+        nickName = request.form['nickName']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        for k in user_dict:
+            if user_dict[k]['email'] == email:
+                return render_template('assignment3_2.html', message2="This user is already exist")
+            else:
+                newUser = {
+                    firstName: {'nickName': nickName, 'lastName': lastName, 'email': email, 'password': password}}
+                user_dict.update(newUser)
+                session['nickName'] = nickName
+                session['logedin'] = True
+                return render_template('assignment3_2.html',
+                                       nickName=nickName,
+                                       firstName=firstName,
+                                       lastName=lastName,
+                                       password=password)
+    return redirect(url_for('assignment3_2'))
 
 
+@app.route('/session')
+def session_func():
+    return jsonify(dict(session))
 
-# @app.route('/registrationForm', methods=['post'])
-# def login_func():
-#     return render_template('log_in.html')
-#
 
+@app.route('/log_out')
+def logout_func():
+    session['logedin'] = False
+    session.clear()
+    return redirect(url_for('assignment3_2'))
 
 
 if __name__ == '__main__':
